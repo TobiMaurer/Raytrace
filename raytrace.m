9c121c5a178229cbc6908fd0f876a61ff0686865
TobiMaurer <TobiMaurer@users.noreply.github.com>
2017-04-30 00:05:48 +0000

    raytrace

--- raytrace.m
+++ raytrace.m
@@ -1,65 +1,69 @@
 % octave -q raytrace.m
 
 
-% define sets
+% object constructors
 
 
-function contained = contained1(coords) % cube/star1
-end
-
-function contained = contained2(coords) % torus/pipe
-end
-
-function contained = contained3(coords) % cone
-end
-
-function contained = contained4(coords) % star2
-end
-
-function contained = contained_test(coords)
-	contained = norm(coords) < 0.5;
-	%return
+function object = new_plane(point,normal,color)
+	normal /= norm(normal);
 	
-	roty = pi/6;
-	rotx = pi/6;
-	coords = coords*[cos(roty),0,sin(roty);0,1,0;-sin(roty),0,cos(roty)];
-	coords = coords*[1,0,0;0,cos(rotx),sin(rotx);0,-sin(rotx),cos(rotx)];
-	
-	if norm(coords) == 0
-		contained = false;
-		return
+	if normal(3) < 0
+		normal *= -1;
 	end
 	
-	direction = coords/norm(coords); %# div 0
-	dist = min([ ...
-		norm(direction*[0,0,0;0,1,0;0,0,1]), ...
-		norm(direction*[1,0,0;0,0,0;0,0,1]), ...
-	]);
+	object.type = "plane";
+	object.point = point;
+	object.normal = normal;
+	object.color = color;
+end
+
+function object = new_sphere(point,radius,color)
+	object.type = "sphere";
+	object.point = point;
+	object.radius = radius;
+	object.color = color;
+end
+
+function object = new_algebraic(point,polynomial3,color)
+	object.type = "algebraic";
+	object.point = point;
+	object.polynomial3 = polynomial3();
+	object.color = color;
+end
+
+
+% define surfaces
+
+
+function polynomial3 = polynomial_algebraic()
+
+	% x^4+y^4+z^4-1
 	
-	factor = ((dist^3)/3-(dist^2)/sqrt(8))*2+1;
-	contained = norm(coords(3)) < 0.35 && norm(coords) < factor^5;
-	
-	%contained = norm(coords,inf) < 0.35;
+	polynomial3(1,1,1) = -1;
+	polynomial3(5,1,1) = 1;
+	polynomial3(1,5,1) = 1;
+	polynomial3(1,1,5) = 1;
 end
 
 
 % define materials
 
 
-function color = color1(coords,normal,curvature) % shiny1
+function color = color_plane(object,point,normal)
+	%#
+	color = abs(mod(point(3)*1,2)-1);
 end
 
-function color = color2(coords,normal,curvature) % furry
-end
+function color = color_sphere(object,point,normal)
+	%#
+	parallel.intensity = 0.5;
+	parallel.direction = [0 0 0];
 
-function color = color3(coords,normal,curvature) % checkered
-end
-
-function color = color4(coords,normal,curvature) % shiny2
-end
-
-function color = color_test(coords,normal,curvature)
 	color = 1+normal(3);
+end
+
+function color = color_algebraic(object,point,normal)
+	%#
 end
 
 
@@ -67,144 +71,217 @@ end
 
 
 clear objects;
-clear ambient;
-clear parallel;
 
-
-objects(1).type = "sphere";
-objects(1).position = [0,0,0];
-objects(1).radius = 1;
-objects(1).transform = eye(4);
-objects(1).color = @color_test;
-
-objects(2).type = "plane";
-objects(2).normal = [0,0,0];
-objects(2).distance = 0.5;
-objects(2).transform = eye(4);
-objects(2).color = @color_test;
-
-objects(3).type = "set";
-objects(3).contained = @contained_test;
-objects(3).transform = eye(4);
-objects(3).color = @color_test;
-
-
-ambient.intensity = 0.5;
-
-parallel.intensity = 0.5;
-parallel.direction = [0,0,0];
+objects{end+1} = new_plane([-1/2 -1/2 -1/2],[1/2 -1 -1/4],@color_plane);
+objects{end+1} = new_sphere([0 0 0],0.9,@color_sphere);
+objects{end+1} = new_sphere([1/2 0 -3/5],0.3,@color_sphere);
+objects{end+1} = new_sphere([-1/2 -1/2 -1/2],0.5,@color_sphere);
+%objects{end+1} = new_algebraic([0 0 0],@polynomial_algebraic,@color_algebraic);
 
 
 % end of scene definition
 
 
-% helper functions
-
-
-function transform = scale(factor)
-end
-
-function transform = translate()
-end
-
-function transform = rotate(pivot,angle)
-end
-
-
 % initialize variables
 
 
-global rows = [-1:2/40:1];
-global cols = [-1:2/40:1];
-global time = [-1:2/20:1];
+global rows = [-1 : 2/80 : 1];
+global cols = [-1 : 2/80 : 1];
 global appx = 10;
 
-depths = ones(numel(rows),numel(cols)) * inf;
-removes = zeros(numel(rows),numel(cols));
-colors = cell(numel(rows),numel(cols));
-normals = zeros(numel(rows),numel(cols),3);
-curvatures = zeros(numel(rows),numel(cols));
-pixels = zeros(numel(rows),numel(cols));
+global objmap = cell(numel(rows),numel(cols));
+global depths = ones(numel(rows),numel(cols)) * inf;
+global pixels = zeros(numel(rows),numel(cols));
 
 
-% determine intersections
+% step 1: determine intersections
 
 
-function depth = depth_sphere(object,row,col)
-	global rows
-	global cols
+function depth = depth_plane(object,coords)
 	depth = inf;
 	
-	if object.transform != 0 %# implement
+	
+	x = object.point - coords;
+	n = object.normal;
+%	v = [0 0 1];
+	
+	
+	% solve linear equation
+	%
+	% 0 = <x - v*t,n>
+	%
+	% 0 = sum_i(x_i*n_i) - n_3*t
+	%
+	% t = sum_i(x_i*n_i)/n_3
+	
+	
+	if n(3) != 0
+		
+		xn = x .* n;    % x_i*n_i
+		s = sum(xn);    % sum_i(x_i*n_i)
+		
+		
+		                % sum_i(x_i*n_i)/n_3
+		depth = s / n(3);
 	end
 end
 
-function depth = depth_plane(object,row,col)
-	global rows
-	global cols
+function depth = depth_sphere(object,coords)
 	depth = inf;
 	
-	if object.transform != 0 %# implement
+	
+	x = object.point - coords;
+	r = object.radius;
+%	v = [0 0 1];
+	
+	
+	% solve quadratic equation
+	%
+	% 0 = |x - v*t| - r
+	%
+	% 0 =   sum_i((v_i)^2)  *t^2  } = t^2
+	%     - sum_i(2*x_i*v_i)*t    } = b*t
+	%     + sum_i((x_i)^2) - r^2  } = c
+	%
+	% t = -b/2 - sqrt(b^2/4 - c)
+	
+	
+	x2 = x .* x;        % (x_i)^2
+	r2 = r * r;         % r^2
+	
+	b = x(3);           % -sum_i(2*x_i*v_i) /-2
+	c = sum(x2) - r2;   %  sum_i((x_i)^2) - r^2
+	
+	b2 = b * b;         % b^2/4
+	d = b2 - c;         % b^2/4 - c
+	
+	
+	if d >= 0
+		                % -b/2 - sqrt(b^2/4 - c)
+		depth = b - sqrt(d);
 	end
 end
 
-function depth = depth_set(object,row,col)
-	global rows
-	global cols
-	global time
-	global appx
-	depth = inf;
+function polynomial = distance(polynomial3,coords)
+	polynomial(size(polynomial3,3)) = 0;
 	
-	depth2 = -inf;
-	for depth1 = time
-		
-		coords = [cols(col),rows(row),depth1];
-		if object.transform != 0
-			coords *= object.transform; %# left mul
-		end
-		
-		if object.contained(coords)
-			for i = 1:appx
+	
+	p = polynomial3;
+	x = coords;
+%	v = [0 0 1];
+	
+	
+	% distance polynomial for x + v*t
+	%
+	% p(x + v*t) =
+	%      sum_ijk(p_ijk*(x_1 + v_1*t)^i
+	%                   *(x_2 + v_2*t)^j
+	%                   *(x_3 + v_3*t)^k)
+	%
+	% q_k = sum_ij(p_ijk*(x^1)^i*(x^2)^j)
+	
+	
+	for i1 = 1:size(p,1)
+		for i2 = 1:size(p,2)
+			for i3 = 1:size(p,3)
 				
-				depth3 = depth1 + depth2;
-				depth3 /= 2;
+				c = p(i1,i2,i3);
+				c *= x(1)^(i1 - 1);
+				c *= x(2)^(i2 - 1);
 				
-				coords = [cols(col),rows(row),depth3];
-				if object.contained(coords)
-					depth1 = depth3;
-				else
-					depth2 = depth3;
-				end
+				polynomial(i3) += c;
+				
 			end
-		
-			depth = depth1;
-			
-			break;
 		end
-		
-		depth2 = depth1;
 	end
+end
+
+function derivative = derive(polynomial)
+	
+	
+	p = polynomial;
+	
+	
+	% derivative of polynomial in t
+	%
+	% q_(i - 1) = p_i*i
+	
+	
+	for i1 = 2:numel(p)
+		
+		c = p(i1);
+		c *= i1 - 1;
+		
+		derivative(i1 - 1) = c;
+		
+	end
+end
+
+function value = evaluate(polynomial,t)
+	value = 0;
+	
+	
+	p = polynomial;
+	
+	
+	% value of polynomial in t
+	%
+	% p(t) = sum_i(p_i*t^i)
+	
+	
+	for i1 = 1:numel(p)
+		
+		v = p(i1);
+		v *= t^(i1 - 1);
+		
+		value += v;
+		
+	end
+end
+
+function depth = depth_algebraic(object,coords)
+	global appx;
+	depth = inf;
+	
+	
+	p = distance(object.polynomial3,coords);
+	q = derive(p);
+	
+	%object.polynomial3
+	p
+	q
+	%exit;
+	
+	x = coords;
+	
+	for i = 1:appx
+	end
+	
+	
+	
+	
+	%#
 end
 
 for row = 1:numel(rows)
 	for col = 1:numel(cols)
 		for object = objects
 			
-			depth = inf;	
+			object = object{:};
+			coords = [cols(col) rows(row) 0];
+			depth = inf;
 			
-			if strcmp(object.type,"sphere")
-				depth = depth_sphere(object,row,col);
-			elseif strcmp(object.type,"plane")
-				depth = depth_plane(object,row,col);
-			elseif strcmp(object.type,"set")
-				depth = depth_set(object,row,col);
+			if strcmp(object.type,"plane")
+				depth = depth_plane(object,coords);
+			elseif strcmp(object.type,"sphere")
+				depth = depth_sphere(object,coords);
+			elseif strcmp(object.type,"algebraic")
+				depth = depth_algebraic(object,coords);
 			end
 			
-			if depth != inf
+			if depth < depths(row,col)
+				objmap{row,col} = object;
 				depths(row,col) = depth;
-				colors{row,col} = object.color;
-			
-				break;
 			end
 			
 		end
@@ -212,126 +289,41 @@ for row = 1:numel(rows)
 end
 
 
-% determine normals for intersections
+% step 2: determine normals and colors
 
 
-for row = 1:numel(rows)
-	for col = 1:numel(cols)
-		
-		if depths(row,col) != inf
-			
-			depth = [inf,inf,inf,inf];
-			normal1 = [0,-1;0,-1]; % x,z;y,z
-		
-			if col > 1
-				depth(1) = depths(row,col - 1);
-				normal1(1,:) = [-1,0];
-			end
-		
-			if col < numel(cols)
-				depth(2) = depths(row,col + 1);
-				normal1(1,:) = [1,0];
-			end
-		
-			if row > 1
-				depth(3) = depths(row - 1,col);
-				normal1(2,:) = [-1,0];
-			end
-		
-			if row < numel(rows)
-				depth(4) = depths(row + 1,col);
-				normal1(2,:) = [1,0];
-			end
-			
-			if (depth(1) == inf && depth(2) == inf) ...
-			|| (depth(3) == inf && depth(4) == inf)
-				removes(row,col) = true;
-			else
-				
-				if depth(1) != inf && depth(2) != inf
-					normal1(1,:) = [ ...
-						depth(2) - depth(1), ...
-						cols(col - 1) - cols(col + 1) ...
-					]; % x=z2-z1,z=x1-x2
-					normal1(1,:) /= norm(normal1(1,:)); %# div 0
-				end
-				
-				if depth(3) != inf && depth(4) != inf
-					normal1(2,:) = [ ...
-						depth(4) - depth(3), ...
-						rows(row - 1) - rows(row + 1) ...
-					]; % y=z2-z1,z=y1-y2
-					normal1(2,:) /= norm(normal1(2,:)); %# div 0
-				end
-				
-				normal2 = [ ...
-					normal1(1,1), ...
-					normal1(2,1), ...
-					min(normal1(1,2),normal1(2,2)) ...
-				]; %# improve
-				
-				
-				normal2x = [ ...
-					normal1(1,1), ...
-					0, ...
-					normal1(1,2) ...
-				]; %# improve
-				
-				
-				normal2 /= norm(normal2); %# div 0
-				
-				normals(row,col,:) = normal2;
-				
-			end
-			
-		end
-	end
+function normal = normal_plane(object,point)
+	normal = object.normal;
 end
 
-
-% remove if normal undefined
-
-
-for row = 1:numel(rows)
-	for col = 1:numel(cols)
-		
-		if removes(row,col)
-			
-			depths(row,col) = inf;
-			
-		end
-		
-	end
+function normal = normal_sphere(object,point)
+	normal = point - object.point;
+	normal /= norm(normal);
 end
 
-
-% determine curvature for intersections
-
-
-for row = 1:numel(rows)
-	for col = 1:numel(cols)
-		
-		if depths(row,col) != inf
-			
-			curvatures(row,col) = 0;
-			
-		end
-	end
+function normal = normal_algebraic(object,point)
+	%#
 end
 
-
-% determine color for intersections
-
-
 for row = 1:numel(rows)
 	for col = 1:numel(cols)
 		
-		if depths(row,col) != inf
+		object = objmap{row,col};
+		depth = depths(row,col);
+		
+		if depth != inf
 			
-			coords = [cols(col),rows(row),depths(row,col)];
+			point = [cols(col) rows(row) depth];
 			
-			pixels(row,col) = colors{row,col} ...
-				(coords,normals(row,col,:),curvatures(row,col));
+			if strcmp(object.type,"plane")
+				normal = normal_plane(object,point);
+			elseif strcmp(object.type,"sphere")
+				normal = normal_sphere(object,point);
+			elseif strcmp(object.type,"algebraic")
+				normal = normal_algebraic(object,point);
+			end
+			
+			pixels(row,col) = object.color(object,point,normal);
 			
 		end
 	end
